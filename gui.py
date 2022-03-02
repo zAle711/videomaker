12a21f1de71744c1d07e96c8dcce755be1680473
zAle711 <alessandro7111@gmail.com>
2022-03-02 21:21:54 +0100

    improved Downloader GUI with image and buttond

--- gui.py
+++ gui.py
@@ -5,3 +5,5 @@
     # def getFile(self):
     #     file_name = QtWidgets.QFileDialog.getOpenFileName(caption="Select text file", filter="Text files (*.txt)")
     #     self.label.setText(file_name[0])
+    # self.label_image = QtWidgets.QLabel(DownloaderWindow)
+    #     self.label_image.setPixmap(QtGui.QPixmap("temp/0.5-(f..) guarda.jpg"))
